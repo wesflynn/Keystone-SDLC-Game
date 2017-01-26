f8f48f959b2700b0716f65ea36024741db920983
adam-bates <info@adambates.ca>
2017-01-26 13:39:24 -0500

    Started building Character and Player classes

--- Keystone-SDLC-Game/src/game/objects/Character.java
+++ Keystone-SDLC-Game/src/game/objects/Character.java
@@ -1,26 +1,61 @@
 package game.objects;
 
 import game.platform.Drawable;
+import javafx.scene.paint.Color;
 
 public abstract class Character implements Drawable
 {
+    private final int WIDTH = 50;
+    private final int HEIGHT = 50;
+    
+    // Global private variables
     private int posX, posY;
+    private Color color;
+    
+    // Accessors
+    public final int getWidth()
+    {
+        return this.WIDTH;
+    }
+    
+    public final int getHeight()
+    {
+        return this.HEIGHT;
+    }
     
     public int getPosX()
     {
         return this.posX;
     }
+    
     public int getPosY()
     {
         return this.posY;
     }
     
+    public Color getColor()
+    {
+        return this.color;
+    }
+    
+    // Mutators
     public void setPosX(int x)
     {
         this.posX = x;
     }
+    
     public void setPosY(int y)
     {
         this.posY = y;
+    }
+    
+    public void setColor(Color color)
+    {
+        this.color = color;
+    }
+    
+    @Override
+    public void draw() {
+        
     }
 }
